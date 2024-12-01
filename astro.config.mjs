415859f5274e300ba64005617208a88cfb1f8337
Stephen Augustus <foo@auggie.dev>
2024-12-01 09:17:37 -0500

    Update site name
    Signed-off-by: Stephen Augustus <foo@auggie.dev>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blahg-bsky.netlify.app/",
+	site: "https://auggie.dev",
 	base: "/",
 	integrations: [sitemap()],
 	markdown: {
